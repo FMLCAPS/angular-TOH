22056dbca09abc99c3632fde004d6c7ae071d941
FMLCAPS <fm.lauze@gmail.com>
2021-06-30 18:39:13 +0200

    HTTPClient implémenté dans le service panier

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,7 +6,12 @@ import { Product } from './products';
   providedIn: 'root'
 })
 export class CartService {
-  constructor(private http: HttpClient) { }
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+  constructor(private http: HttpClient) {}
 
   items: Product[] = [];
 
