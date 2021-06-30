baa79fa261bea7d3172052abf6c9f4180f52284b
FMLCAPS <fm.lauze@gmail.com>
2021-06-30 18:38:04 +0200

    Configuration utilisation HTTPService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,17 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
@@ -27,8 +29,7 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent,
-
+    CartComponent
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   items: Product[] = [];
 
